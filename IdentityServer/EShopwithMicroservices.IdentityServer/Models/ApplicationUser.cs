73efa6cfa0483ce54f6252d12b92be637a77561c
ercan gun <e.guntas@windowslive.com>
2025-03-19 23:57:24 +0300

    Add IdentityServer

---
+++ IdentityServer/EShopwithMicroservices.IdentityServer/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EShopwithMicroservices.IdentityServer.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
