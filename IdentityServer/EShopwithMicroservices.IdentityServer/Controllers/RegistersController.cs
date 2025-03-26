49a15c3c628813788c23b835086476c93eb4fa05
ercan gun <e.guntas@windowslive.com>
2025-03-27 00:25:21 +0300

    Identityserver setup

---
+++ IdentityServer/EShopwithMicroservices.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,37 @@
+﻿using EShopwithMicroservices.IdentityServer.Dtos;
+using EShopwithMicroservices.IdentityServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EShopwithMicroservices.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterDto userRegisterDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = userRegisterDto.Username,
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname
+            };
+            var result = await _userManager.CreateAsync(user, userRegisterDto.Password);
+            if (result.Succeeded)
+            {
+                return Ok("User added succesful");
+            }
+            return BadRequest();
+        }
+    }
+}
