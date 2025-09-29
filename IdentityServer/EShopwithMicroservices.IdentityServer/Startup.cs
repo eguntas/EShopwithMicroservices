3de319e67553e039bc002f0987932e0026ffbc36
ercan gun <e.guntas@windowslive.com>
2025-09-29 17:12:13 +0300

    IdentityServer Configuration

--- IdentityServer/EShopwithMicroservices.IdentityServer/Startup.cs
+++ IdentityServer/EShopwithMicroservices.IdentityServer/Startup.cs
@@ -49,6 +49,7 @@ namespace EShopwithMicroservices.IdentityServer
             })
                 .AddInMemoryIdentityResources(Config.IdentityResources)
                 .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryApiResources(Config.ApiResources)
                 .AddInMemoryClients(Config.Clients)
                 .AddAspNetIdentity<ApplicationUser>();
 
