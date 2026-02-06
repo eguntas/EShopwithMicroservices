f09d8070684191ce8c42b3a896298b1b134577a2
ercan gun <e.guntas@windowslive.com>
2026-02-06 12:26:10 +0300

    Develop Admin Area Comment Page

---
+++ UI/MultiShop.WebUI/Areas/Admin/Controllers/CommentController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CommentDtos;
+using System.Text;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    [Route("Admin/Comment")]
+    public class CommentController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public CommentController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v1 = "Home Page";
+            ViewBag.v2 = "Comments";
+            ViewBag.v3 = "Comments List";
+            ViewBag.v0 = "Comments Transaction";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7200/api/Comments");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = Newtonsoft.Json.JsonConvert.DeserializeObject<List<ResultCommentDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+
+
+        [Route("DeleteComment/{id}")]
+        public async Task<IActionResult> DeleteComment(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7200/api/Comments?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Comment", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("UpdateComment/{id}")]
+        public async Task<IActionResult> UpdateComment(string id)
+        {
+            ViewBag.v1 = "Home Page";
+            ViewBag.v2 = "Comments";
+            ViewBag.v3 = "Comments List";
+            ViewBag.v0 = "Comments Transaction";
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7200/api/Comments/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = Newtonsoft.Json.JsonConvert.DeserializeObject<UpdateCommentDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [Route("UpdateComment/{id}")]
+        public async Task<IActionResult> UpdateComment(UpdateCommentDto dtos)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(dtos);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7200/api/Comments/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Comment", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
