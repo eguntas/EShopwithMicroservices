77ae84ce58afd47dedda6085fb7761da307b9a63
ercan gun <e.guntas@windowslive.com>
2026-02-09 12:32:13 +0300

    Comment Integrated WEB UI

--- UI/MultiShop.WebUI/Controllers/ProductController.cs
+++ UI/MultiShop.WebUI/Controllers/ProductController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CommentDtos;
+using System.Text;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ProductController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index(string id)
         {
             ViewBag.CategoryId = id;
@@ -13,6 +20,27 @@ namespace MultiShop.WebUI.Controllers
         public IActionResult ProductDetail(string id)
         {
             ViewBag.ProductId = id;
+            return View();
+        }
+        [HttpGet]
+        public PartialViewResult AddComment()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddComment(CreateCommentDto createCommentDto)
+        {
+            createCommentDto.ProductId = createCommentDto.ProductId;    
+            createCommentDto.Status = false;
+            createCommentDto.CreatedDate = DateTime.Now;
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(createCommentDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44365/api/Comments", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
             return View();
         }
     }
