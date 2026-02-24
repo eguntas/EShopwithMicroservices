24f181505f890033faf8c90dc7829dc094cb9ca2
ercan gun <e.guntas@windowslive.com>
2026-02-25 00:54:21 +0300

    Rabbitmq

---
+++ RabbitMqMessage/EShop.RabbitMqMessage/Controllers/MessageController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace EShop.RabbitMqMessage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateMessage()
+        {
+            var connectionFactory = new ConnectionFactory() 
+            { 
+                HostName = "localhost"
+            };
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("Quequ1" , false , false , false , null);
+            var msgContent = "rabbitmq queue message";
+            var bytemessage = Encoding.UTF8.GetBytes(msgContent);
+            channel.BasicPublish(exchange:"" , routingKey: "Quequ1" , basicProperties:null,body:bytemessage);
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult ReadMessage()
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, x) =>
+            {
+                var byteMessage = x.Body.ToArray();
+                var message = Encoding.UTF8.GetString(byteMessage);
+            };
+            channel.BasicConsume(queue: "Quequ1", autoAck: false, consumer: consumer);
+            return Ok();
+        }
+    }
+}
---
+++ Services/Images/MultiShop.Images/Controllers/ImageUploadController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.Images.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageUploadController : ControllerBase
+    {
+    }
+}
